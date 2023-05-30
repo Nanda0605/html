dbf5ff15f9aae6e08b0077101ccbd1384cba2c25
Nanda0605 <nandupedavalli@gmail.com>
2023-05-30 11:27:56 +0530

    initial

---
+++ firstprogram.js
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <body>
+        </body>
+        <script>
+            let a="nandavardhan"
+            function myfun(){
+                let b="hi"
+                document.write(b+"<br>")
+            }
+            myfun()
+            document.write("outside the function"+a+"<br>")
+        </script>
+    </head>
+</html>
